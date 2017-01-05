34b7f6b6ddedf37fe5ed921cc2c99d5026b8a7b9
Nathaniel Buechler <natebuechler@gmail.com>
2017-01-04 22:35:13 -0500

    feat: Process Gutenberg corpus from NLTK

--- app/helpers/views.py
+++ app/helpers/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask import render_template, redirect, url_for, jsonify, request
 
+from nltk.corpus import gutenberg
+
 import controllers
 
 helpers = Blueprint('helpers', __name__)
@@ -63,6 +65,43 @@ def save_many_full_records(collection_name):
     for doc in docs:
         data = {
             "doc": doc,
+            "lang": lang,
+            "upper_bound": upper_bound,
+            "lower_bound": lower_bound,
+            "natural": natural,
+            "stemmer": stemmer,
+            "lemma": lemma,
+        }
+        controllers.save_record(collection_name, data)
+
+    return "Success"
+
+'''
+save_full_records_from_gutenberg
+===
+This method looks at gutenberg from NLTK and saves the records to use as
+training/testing data for ML purposes
+'''
+@helpers.route('/save-gutenberg-records/<collection_name>/', methods=['POST'])
+def save_full_records_from_gutenberg(collection_name):
+    r = request.get_json()
+
+    corpus_name = r.get('corpus_name')
+    lang = r.get('lang')
+    upper_bound = r.get('ub')
+    lower_bound = r.get('lb')
+    # TODO: Add Error Handling
+    natural = r.get('natural')
+    stemmer = r.get('stemmer')
+    lemma = r.get('lemma')
+
+    guten_sents = gutenberg.sents(corpus_name + '.txt')
+
+    for sent in guten_sents:
+        sent = ' '.join(sent)
+        print sent
+        data = {
+            "doc": sent,
             "lang": lang,
             "upper_bound": upper_bound,
             "lower_bound": lower_bound,
