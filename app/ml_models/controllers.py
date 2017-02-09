71ca8326007a66d1e25b6556c05a8858efb1f3f8
Brian Coe <bwc126@gmail.com>
2017-02-08 23:53:02 -0500

    feat: Add learning curve plot to train_model()
    Please advise on better location or structure for learning curve production. Further plots will be added for model tuning.

--- app/ml_models/controllers.py
+++ app/ml_models/controllers.py
@@ -1,9 +1,11 @@
 from flask import jsonify
 import requests, operator, math, json, csv, os
+import matplotlib.pyplot as plt
 
 from sklearn.tree import DecisionTreeRegressor as DTR
 from sklearn.model_selection import ShuffleSplit
 from sklearn.model_selection import cross_val_score as CVS
+from sklearn.model_selection import learning_curve
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
@@ -60,13 +62,85 @@ def train_model():
     X,y = prepare_data(True)
 
     print X[:1000]
-    split = ShuffleSplit(n_splits=10, test_size=0.30, random_state=42)
+    split = ShuffleSplit(n_splits=3, test_size=0.30, random_state=42)
     t_1 = time.clock()
-    reg = DTR()
-    scorer = CVS(reg, X[:1000], y[:1000], cv=split, pre_dispatch=3)
-    for score in scorer:
-        print score
+    estimator = DTR()
 
+    # scorer = CVS(reg, X[:1000], y[:1000], cv=split, pre_dispatch=3)
+    # for score in scorer:
+        # print score
+
+
+    def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+        """
+        Generate a simple plot of the test and training learning curve.
+
+        Parameters
+        ----------
+        estimator : object type that implements the "fit" and "predict" methods
+            An object of that type which is cloned for each validation.
+
+        title : string
+            Title for the chart.
+
+        X : array-like, shape (n_samples, n_features)
+            Training vector, where n_samples is the number of samples and
+            n_features is the number of features.
+
+        y : array-like, shape (n_samples) or (n_samples, n_features), optional
+            Target relative to X for classification or regression;
+            None for unsupervised learning.
+
+        ylim : tuple, shape (ymin, ymax), optional
+            Defines minimum and maximum yvalues plotted.
+
+        cv : int, cross-validation generator or an iterable, optional
+            Determines the cross-validation splitting strategy.
+            Possible inputs for cv are:
+              - None, to use the default 3-fold cross-validation,
+              - integer, to specify the number of folds.
+              - An object to be used as a cross-validation generator.
+              - An iterable yielding train/test splits.
+
+            For integer/None inputs, if ``y`` is binary or multiclass,
+            :class:`StratifiedKFold` used. If the estimator is not a classifier
+            or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+            Refer :ref:`User Guide <cross_validation>` for the various
+            cross-validators that can be used here.
+
+        n_jobs : integer, optional
+            Number of jobs to run in parallel (default 1).
+        """
+        plt.figure()
+        plt.title(title)
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.xlabel("Training examples")
+        plt.ylabel("Score")
+        train_sizes, train_scores, test_scores = learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+        train_scores_mean = np.mean(train_scores, axis=1)
+        train_scores_std = np.std(train_scores, axis=1)
+        test_scores_mean = np.mean(test_scores, axis=1)
+        test_scores_std = np.std(test_scores, axis=1)
+        plt.grid()
+
+        plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+        plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1, color="g")
+        plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+        plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+
+        plt.legend(loc="best")
+        return plt
+
+    title = "Learning Curves (stock DTR)"
+    plot_learning_curve(estimator, title, X[:3000], y[:3000], (0.1, 1.01), split, n_jobs=1)
+    plt.show()
     t_2 = time.clock()
     print "Total time: ", t_2-t_1
     return 'training model'
