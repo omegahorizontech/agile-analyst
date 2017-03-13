37eb2abcc9bae94be4fdda2000842d6f244b5484
Brian Coe <bwc126@gmail.com>
2017-03-12 23:49:59 -0400

    feat: Modify DTR parameters, use 3 columns to assess scaling

--- app/ml_models/controllers.py
+++ app/ml_models/controllers.py
@@ -83,13 +83,13 @@ def train_model():
     X,y = prepare_data(True)
     # print y.shape
     # y = y[y.columns[30:90]]
-    y = y[y.columns[30:36]]
+    y = y[y.columns[30:33]]
 
     print 'this should be y', y
     # print X[:1000]
     split = ShuffleSplit(n_splits=1, test_size=0.09, random_state=42)
     t_1 = time.clock()
-    estimator = DTR(max_features=0.99, max_depth=11, random_state=12, splitter='random', min_samples_split=.003, presort=True)
+    estimator = DTR(max_features=.999, max_depth=12, random_state=12, splitter='random', min_samples_split=.0012, presort=True)
 
     estimator3 = RFR(n_estimators=2, max_features=0.33, n_jobs=-1)
 
@@ -179,7 +179,7 @@ def train_model():
     # plot_learning_curve(estimator2, title, X[:2000], y[:2000], (0.1, 1.01), split, n_jobs=1)
     # plt.show()
 
-    title = "Learning Curves (DTR(depth 11, 0.99 features, random splits, min split .003, presort)+MOR, 12k samples, 0.09 test)"
+    title = "Learning Curves (DTR(depth 12, 0.999 features, random splits, min split .0012, presort)+MOR, 12k samples, 0.09 test, 3 columns)"
     plot_learning_curve(estimator8, title, X[:12000], y[:12000], (-0.1, 1.01), n_jobs=-1, cv=split)
     plt.show()
 
