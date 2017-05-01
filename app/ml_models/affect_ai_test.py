30a2779a0b0d5ff65ebe565e18ca913629d8e00d
Brian Coe <bwc126@gmail.com>
2017-05-01 17:27:40 -0400

    feat: Add pytest to project reqs and appropriate file

--- app/ml_models/affect_ai_test.py
+++ app/ml_models/affect_ai_test.py
@@ -1,7 +1,14 @@
 import affect_ai
-
-# Test that an affect_AI object gets created correctly
-# Test that an affect_AI object can be trained, and builds vocabulary correctly
-# Test that an affect_AI object correctly scores samples
+import pytest
 
 # words: foo, bar, baz, goo, car, caz, hoo, dar, daz, ioo, ear, eaz, loo, far, faz; corpora: happiness 1, satisfaction 2, elation 3
+
+# Test that an affect_AI object gets created correctly
+def test_creation():
+    pass 
+# Test that an affect_AI object can be trained, and builds vocabulary correctly
+def test_training():
+    pass
+# Test that an affect_AI object correctly scores samples
+def test_scoring():
+    pass
