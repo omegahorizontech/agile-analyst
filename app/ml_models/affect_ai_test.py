b356cd07fdeb249ff91160a0961b9cc660c7ae01
Brian Coe <bwc126@gmail.com>
2017-04-30 20:53:08 -0400

    chore: Add test file for affect_ai class

---
+++ app/ml_models/affect_ai_test.py
@@ -0,0 +1,7 @@
+import affect_ai
+
+# Test that an affect_AI object gets created correctly
+# Test that an affect_AI object can be trained, and builds vocabulary correctly
+# Test that an affect_AI object correctly scores samples
+
+# words: foo, bar, baz, goo, car, caz, hoo, dar, daz, ioo, ear, eaz, loo, far, faz; corpora: happiness 1, satisfaction 2, elation 3
